c26fd00e61073aabd69a160721369f19630b2ea0
peteyb37 <ptbudd@gmail.com>
2024-09-04 03:35:11 -0600

    Updated Projects and Page title

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Container, CssBaseline, Grid, Typography, Box } from '@mui/material';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Home from './components/Home';
 import Header from './components/header';
 import Footer from './components/Footer';
@@ -15,6 +15,12 @@ import "./App.css";
 import SuspenseWrapper from "./components/SuspenseWrapper";
 
 function App() {
+
+  useEffect(() => {
+    document.title="BUDD Dev"
+  },[])
+
+
   return (
     <div>
       <BrowserRouter basename='/'>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,19 @@
 import { Box, Typography, Fab, Divider  } from '@mui/material';
+import React, { useState } from 'react';
 
 function Projects() {
+
+  const [aiSynthOpen, setAiSynthOpen] = useState(false);
+
   return (
     <Box sx={{borderTop: '3px solid #912F40', display: 'flex', flexDirection: 'column', minHeight: '80vh', backgroundColor: 'black'}}>
-      <Box sx={{border: '3px solid #FFFFFA', marginLeft: '10%', marginRight: '10%', marginTop: '25px', color: '#FFFFFA', display: 'flex', flexDirection: 'row'}}>
+      <Box sx={{marginTop: '20px', width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+        <Fab onClick={() => window.open('https://github.com/peteyb37/peteyb37.github.io?tab=readme-ov-file', '_blank')} variant='extended' sx={{width: '300px', borderRadius: '10px', color: '#FFFFFA', backgroundColor: 'black', border: '2px solid #912F40', "&:hover":{backgroundColor: '#702632'}}}>Website Github</Fab>
+      </Box>
+      <Box sx={{marginTop: '20px', width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
+        <Fab onClick={() => setAiSynthOpen(!aiSynthOpen)} variant='extended' sx={{width: '300px', borderRadius: '10px', color: '#FFFFFA', backgroundColor: 'black', border: '2px solid #912F40', "&:hover":{backgroundColor: '#702632'}}}>AI Synth MK I</Fab>
+      </Box>
+      {aiSynthOpen && (<Box sx={{border: '3px solid #FFFFFA', marginLeft: '10%', marginRight: '10%', marginTop: '25px', color: '#FFFFFA', display: 'flex', flexDirection: 'row'}}>
         <Box sx={{
           height: "400px",
           width: "60%",
@@ -30,7 +40,7 @@ function Projects() {
 
           </Box>
         </Box>
-      </Box>
+      </Box>)}
     </Box>
   )
 }
